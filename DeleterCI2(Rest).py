ec7d12ac03fb7fdcd59c440fbab1546e9a9224e4
Кувалин Влад <59653793+Vkuvalin@users.noreply.github.com>
2022-05-27 09:50:38 +0300

    Add files via upload

---
+++ DeleterCI2(Rest).py
@@ -0,0 +1,77 @@
+"""
+Скрипт предназначен для создания подключения к UCMDB через Rest Api: создания КЕ и удаления КЕ.
+"""
+
+#coding=utf-8
+
+import rest_requests as requests
+import logger
+from appilog.common.system.types.vectors import ObjectStateHolderVector
+
+
+def execute_post(url, urn, headers, body):
+    uri = url + urn
+    r = requests.post(uri, headers=headers, json=body, verify=False)
+    logger.debug('_________________POST request results___________')
+    res = r.json()
+    return res
+
+
+def authenticate(url):
+    urn = "/rest-api/authenticate"
+    headers = {"Content-Type": "application/json"}
+    body={"username": "user", "password": "passwd", "clientContext": 1}
+    out = execute_post(url, urn, headers, body)
+    # out = r.json()
+    token = out["token"]
+    headers = {"Authorization": "Bearer " + token, "Content-Type": "application/json; charset=utf-8"}
+    return headers
+
+"""
+# Создание пустых КЕ 
+def creating():
+    for i in range(5000):
+        body = {"cis": [{"ucmdbId": "1",
+                         "type": "node",
+                         "properties": {"name": "test_node{}".format(i)},
+                         "attributesQualifiers": {}
+                         }]}
+        res = execute_post("/rest-api/dataModel", headers, body)
+"""
+
+
+def DiscoveryMain(Framework):
+    OSHVResult = ObjectStateHolderVector()
+    try:
+        #change hardcode to get credentials
+        protocol = "https"
+        port = "8443"
+        host = "10.3.6.78"
+
+        url = protocol + '://' + host + ":" + port
+
+        headers = authenticate(url)
+
+        body = {"name": "get_nodes_to_delete",
+            "nodes": [{"queryIdentifier": "nodes",
+                    "type": "node",
+                    "visible": True,
+                    "includeSubtypes": True,
+                    "layout": ["display_label", "global_id"],
+                    "ids": [],}, ],}
+
+        res = execute_post(url, "/rest-api/topologyQuery/", headers, body)
+        for ci in res["cis"]:
+            ciId = ci["ucmdbId"]
+            del_ci_uri = protocol + '://' + host + ":" + port + "/rest-api/dataModel/ci/" + ciId
+            r = requests.delete(del_ci_uri, headers=headers, verify=False)
+            if r.status_code != 200:
+                logger.debug("___Couldnt delete CI ___", ci2id)
+                logger.debug("status code", r.status_code)
+                res = r.json()
+                logger.debug(res)
+    except Exception, e:
+        logger.debug("smth was wrong ", e)
+
+
+    return OSHVResult
